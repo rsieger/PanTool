59d037cd22344f2675aa13fda69b59a1f3449dc4
Rainer Sieger <rsieger@pangaea.de>
2016-06-10 17:55:27 +0200

    macdeploy - Qt 5.6.0 -> Qt 5.6.1

--- macdeploy.sh
+++ macdeploy.sh
@@ -2,12 +2,12 @@
 echo
 echo PanTool
 
-echo - macdeployqt 
+echo - macdeployqt
 
 cd ~/Development/GitHub/PanTool
 
 rm -R '../../Distribution/PanTool/PanTool.app'
-cp -R './build-PanTool-Desktop_Qt_5_6_0_clang_64bit-Release/PanTool.app' '../../Distribution/PanTool/PanTool.app'
+cp -R './build-PanTool-Desktop_Qt_5_6_1_clang_64bit-Release/PanTool.app' '../../Distribution/PanTool/PanTool.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanTool/PanTool.app/Contents/Info.plist'
 
 /Developer/Qt/5.6/clang_64/bin/macdeployqt '../../Distribution/PanTool/PanTool.app'
