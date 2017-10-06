3ca68272e467efc527f06bb656c41213717e33c7
Rainer Sieger <rsieger@pangaea.de>
2017-10-06 19:03:37 +0200

    Qt 5.9.1 -> Qt 5.9.2

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,10 +7,10 @@ echo - macdeployqt
 cd ~/Development/GitHub/PanTool
 
 rm -R '../../Distribution/PanTool/PanTool.app'
-cp -R './build-PanTool-Desktop_Qt_5_9_1_clang_64bit-Release/PanTool.app' '../../Distribution/PanTool/PanTool.app'
+cp -R './build-PanTool-Desktop_Qt_5_9_2_clang_64bit-Release/PanTool.app' '../../Distribution/PanTool/PanTool.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanTool/PanTool.app/Contents/Info.plist'
 
-/Developer/Qt/5.9.1/clang_64/bin/macdeployqt '../../Distribution/PanTool/PanTool.app'
+/Developer/Qt/5.9.2/clang_64/bin/macdeployqt '../../Distribution/PanTool/PanTool.app'
 
 echo - code signing
 
